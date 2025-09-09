dd88b6839488d02853d25bf934913acaecbb3479
Elina <elina@MacBook-Pro-Elina.local>
2025-09-09 19:49:42 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let result = 0;
+  const rentDay = 40;
+  const sum = rentDay * days;
+
+  if (days >= 7) {
+    result = sum - 50;
+  } else if (days >= 3) {
+    result = sum - 20;
+  } else {
+    return sum;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
