f6ba26751f94fe0215587af4f70c4f32fb5fdffe
Elina <elina@MacBook-Pro-Elina.local>
2025-09-09 20:24:54 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,36 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let result = 0;
-  const rentDay = 40;
-  const sum = rentDay * days;
+  //   let result = 0;
+  //   const rentDay = 40;
+  //   const sum = rentDay * days;
 
-  if (days >= 7) {
-    result = sum - 50;
-  } else if (days >= 3) {
-    result = sum - 20;
-  } else {
-    return sum;
+  //   if (days >= 7) {
+  //     result = sum - 50;
+  //   } else if (days >= 3) {
+  //     result = sum - 20;
+  //   } else {
+  //     return sum;
+  //   }
+
+  //   return result;
+  // }
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return result;
-}
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
 
+  return basePrice;
+}
 module.exports = calculateRentalCost;
